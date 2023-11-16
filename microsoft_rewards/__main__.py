e65103a71796ba6aeb18f9be03d86ba518cbf582
areif-dev <ajreifsnyder@protonmail.com>
2023-11-16 18:08:03 -0500

    Call handle_daily_set in main after login

--- microsoft_rewards/__main__.py
+++ microsoft_rewards/__main__.py
@@ -1,5 +1,7 @@
 from microsoft_rewards.authentication import microsoft_login
+from microsoft_rewards.rewards import handle_daily_set
 
 
 if __name__ == "__main__":
     microsoft_login()
+    handle_daily_set()
